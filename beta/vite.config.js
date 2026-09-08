5ba2b8e5c608036e7c35a0a1da5dbf6806e716c9
DaamHaam <63154077+DaamHaam@users.noreply.github.com>
2026-09-08 13:57:40 +0000

    Deploying to gh-pages from @ DaamHaam/Orange-Repondeur@f6ba169e77d5766cd3ae3af3917396cb98873fae 🚀

--- beta/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-const repoSlug = 'Orange-Repondeur';
-
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE || `/${repoSlug}/`,
-});
